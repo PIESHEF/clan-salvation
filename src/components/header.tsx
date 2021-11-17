c8dd72a4ff39b2f064271674ac4f4170aa1d48c0
Alex Pieshefski <apieshefski@gmail.com>
2021-11-17 11:04:35 -0500

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,3 +12,17 @@ export const Header = () => {
         </div>
     )
 }
+
+// <header class='p-4 m-2 bg-gray-400 rounded-3xl'>
+//   <nav class="grid grid-cols-3 items-center place-content-evenly">
+//     <a class="px-4">Salvation</a>
+//     <ul class="px-4">
+//       <li class="inline-block px-3">About</li>
+//       <li class="inline-block px-3">Roster</li>
+//       <li class="inline-block px-3">Hall of Fame</li>
+//     </ul>
+//     <div class="px-5 justify-self-end">
+//       <button class="px-4 py-1 bg-blue-700 shadow-lg rounded-full">Discord</button>
+//     </div>
+//   <nav>
+// </header>
