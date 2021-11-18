59e3e512305d72d87bf0b7ee9c50b3139ec21282
Alex Pieshefski <apieshefski@gmail.com>
2021-11-18 09:48:51 -0500

    Styled navbar pages text

--- src/components/pages.tsx
+++ src/components/pages.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const Page= (props: any) => {
+export const Page = (props: any) => {
     return (
         <>
-            <li className="inline-block px-3 text-m antialiased">
+            <li className="cursor-pointer inline-block px-3 text-m font-bold text-gray-500 hover:text-gray-600 antialiased">
                 {props.label}
             </li>
         </>
